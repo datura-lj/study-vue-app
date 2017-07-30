1ed3cc8d5f113d1fb740d4a6f28aedc9c1312344
datura_lj <datura_lj@163.com>
2017-07-30 19:56:15 +0800

    左边栏

---
+++ src/components/navtools/LeftTabBar.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="left-bar">
+		<section v-for="(item,index) in leftTabData" :key="index">
+      <h3>{{item.name}}</h3>
+      <p>{{item.path}}</p>
+    </section>
+	</div>
+</template>
+<script>
+export default{
+  props:['leftTabData']
+}
+</script>
+<style scoped lang="scss" rel="stylesheet/scss" type="text/css">
+	.left-bar{
+		width: 25%;
+		height: 100%;
+		background-color: aqua;
+    position: absolute;
+    left: 0;
+    top: 0;
+	}
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,19 +1,40 @@
 <template>
 	<div class="home">
-		<h2>我是home</h2>
-		<nav-tools class="nav-tools"></nav-tools>
+		<h2>vue</h2>
+		<left-tab :leftTabData="leftTabData"></left-tab>
+		<nav-tools v-show="toolState"></nav-tools>
 	</div>
 </template>
 <script>
-import Navtab from '../components/navtools/Navtab'
+import NavTab from '../components/navtools/Navtab'
+import LeftTab from '../components/navtools/LeftTabBar'
 export default {
+	data(){
+		return{
+			toolState: true,
+      leftTabData: [
+        {
+          "name": '基础语法',
+          "path": 'base',
+        },
+        {
+          "name": '进阶',
+          "path": 'bbb',
+        }
+      ]
+		}
+	},
 	components: {
-		'nav-tools': Navtab
+		'nav-tools': NavTab,
+		'left-tab': LeftTab
 	}
 }
 </script>
 <style lang='scss' rel="stylesheet/scss" type="text/css" scoped>
-   .nav-tools{
-   		
+   .home{
+      width: 100%;
+      height: 100%;
+   		position: relative;
+     background-color: pink;
    }
 </style>
